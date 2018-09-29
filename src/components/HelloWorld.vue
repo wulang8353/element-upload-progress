8c85660bc2b45b0c2c54ce6d83e4830269b935e5
wulang5 <wulang5@hikvision.com.cn>
2018-09-29 18:16:57 +0800

    增加开发环境配置

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,31 @@
 <template>
-  <div>11</div>
+  <div>
+    <el-container>
+      <el-header>导入</el-header>
+      <el-main>
+        <div class="content">
+          <div class="alert">
+            <el-alert title="警告提示的文案" show-icon :closable="false" style="backgroundColor: #409EFF;color:#fff">
+            </el-alert>
+          </div>
+
+          <!-- 第一阶段： 准备导入 -->
+          <div class="page-import-ready" v-show="fileImport.status === status.ready">
+
+          </div>
+        </div>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
+      fileImport:{
+        status
+      }
     };
   },
 
@@ -13,11 +33,32 @@ export default {
 
   computed: {},
 
-  mounted: {},
-
   methods: {}
 }
 
 </script>
-<style scoped>
+<style  lang="less">
+.el-container {
+  .el-header {
+    color: red;
+  }
+  .content {
+    .el-alert {
+      width: 880px;
+      margin: 0 auto;
+      margin-bottom: 24px;
+      .el-alert__icon {
+        font-size: 16px;
+        width: 16px;
+        color: #fff;
+        display: table-cell;
+      }
+      .el-alert__content {
+        display: table-cell;
+        line-height: 0px;
+        padding: 0 16px 0 8px;
+      }
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import Element from 'element-ui'
 
+import 'element-ui/lib/theme-chalk/index.css';
+
 Vue.config.productionTip = false
 
 Vue.use(Element)
